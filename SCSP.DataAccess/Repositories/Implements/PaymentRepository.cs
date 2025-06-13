6defbd6d626bdddc66b94f6db989e8a9c42eb357
Tien Trong <trongtiensp007@gmail.com>
2025-06-13 20:38:22 +0700

    Add base crud for payments, quit_plans,  smoking_records

---
+++ SCSP.DataAccess/Repositories/Implements/PaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using BusinessObjects.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using SCSP.DataAccess.Repositories.Interfaces;
+
+namespace SCSP.DataAccess.Repositories.Implements
+{
+    public class PaymentRepository : CrudRepository<Payment>, IPaymentRepository
+    {
+        public PaymentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ SCSP.DataAccess/Repositories/Implements/SmokingRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObjects.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using SCSP.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SCSP.DataAccess.Repositories.Implements
+{
+    public class SmokingRecordRepository : CrudRepository<SmokingRecord>, ISmokingRecordRepository
+    {
+        public SmokingRecordRepository(DbContext context) : base(context)
+        {
+        }   
+    }
+}
