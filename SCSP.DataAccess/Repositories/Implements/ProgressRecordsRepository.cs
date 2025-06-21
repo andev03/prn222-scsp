a8a571c0be5590d3b1af2add97eb10997861d442
andev03 <nguyenhoangan03.work@gmail.com>
2025-06-21 10:16:04 +0700

    refector: set project as start

---
+++ SCSP.DataAccess/Repositories/Implements/ProgressRecordsRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Models.Entities;
+using SCSP.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCSP.DataAccess.Repositories.Implements
+{
+    public class ProgressRecordsRepository : IProgressRecordsRepository
+    {
+        ProgressRecord GetById()
+        {
+
+        }
+    }
+}
