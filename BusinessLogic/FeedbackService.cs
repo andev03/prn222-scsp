58aa4205a168a0327950cae2f8c9f13938d1633b
andev03 <nguyenhoangan03.work@gmail.com>
2025-07-23 00:12:35 +0700

    done feedback

--- BusinessLogic/FeedbackService.cs
+++ BusinessLogic/FeedbackService.cs
@@ -13,10 +13,12 @@ namespace BusinessLogic
     public class FeedbackService : IFeedbackService
     {
         private readonly IFeedbackRepository _feedbackRepository;
+        private readonly IUserRepostitory _userRepostitory;
 
-        public FeedbackService(IFeedbackRepository feedbackRepository)
+        public FeedbackService(IFeedbackRepository feedbackRepository, IUserRepostitory userRepostitory)
         {
             _feedbackRepository = feedbackRepository;
+            _userRepostitory = userRepostitory;
         }
 
         public void Create(Feedback feedback)
@@ -26,7 +28,27 @@ namespace BusinessLogic
 
         public List<CoachDto> GetAll()
         {
-            return _feedbackRepository.GetAll();
+            List<User> users = _userRepostitory.GetAllByRole(); // List<User> có Role = "coach"
+            List<CoachDto> coachDtos = _feedbackRepository.GetAll(); // List<CoachDto> đã có feedback
+            List<CoachDto> result = new List<CoachDto>();
+
+            // Lấy danh sách các CoachID đã có feedback
+            var coachIdsWithFeedback = coachDtos.Select(c => c.CoachID).ToHashSet();
+
+            // Lặp qua tất cả user, nếu chưa có feedback thì thêm vào result
+            foreach (var user in users)
+            {
+                if (!coachIdsWithFeedback.Contains(user.UserId))
+                {
+                    coachDtos.Add(new CoachDto
+                    {
+                        CoachID = user.UserId,
+                        Name = user.FirstName + " " + user.LastName,
+                        Rating = 0 // hoặc null nếu muốn phân biệt
+                    });
+                }
+            }
+            return coachDtos;
         }
 
         public List<Feedback> GetByCoachId(Guid coachId)
